7a1c82d80bfaf383d86cdef97429e5c2b4220b70
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-09-18 14:13:05 -0500

    Avance

---
+++ cmake-build-debug/CMakeFiles/mutant-flatword-explorers-118-AlejandroMamaniAndia199802.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mutant-flatword-explorers-118-AlejandroMamaniAndia199802.dir/main.cpp.obj"
+  "mutant-flatword-explorers-118-AlejandroMamaniAndia199802.pdb"
+  "mutant-flatword-explorers-118-AlejandroMamaniAndia199802.exe"
+  "mutant-flatword-explorers-118-AlejandroMamaniAndia199802.exe.manifest"
+  "libmutant-flatword-explorers-118-AlejandroMamaniAndia199802.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mutant-flatword-explorers-118-AlejandroMamaniAndia199802.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
